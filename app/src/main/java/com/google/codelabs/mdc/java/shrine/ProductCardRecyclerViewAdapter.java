7595c2fd4887976caf873a2f31571403ed36d5f7
gomezfox <gomezfox@uw.edu>
2019-12-15 23:11:49 -0800

    Added recyclerview

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardRecyclerViewAdapter.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardRecyclerViewAdapter.java
@@ -3,18 +3,19 @@ package com.google.codelabs.mdc.java.shrine;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.codelabs.mdc.java.shrine.network.ImageRequester;
 import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
 
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
 /**
  * Adapter used to show a simple grid of products.
  */
+
+
 public class ProductCardRecyclerViewAdapter extends RecyclerView.Adapter<ProductCardViewHolder> {
 
     private List<ProductEntry> productList;
--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
@@ -1,13 +1,12 @@
 package com.google.codelabs.mdc.java.shrine;
 
-import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
 
 public class ProductCardViewHolder extends RecyclerView.ViewHolder {
 
     public ProductCardViewHolder(@NonNull View itemView) {
         super(itemView);
-        //TODO: Find and store views from itemView
     }
 }
